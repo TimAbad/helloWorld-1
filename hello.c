e829835c056aea75c6eac1d2a69c1079b26dec97
TimAbad <timothy.abad.611@my.csun.edu>
2020-11-15 11:44:43 -0800

    test commit

--- hello.c
+++ hello.c
@@ -22,7 +22,11 @@ a = 10;
 b = 20;
 
 c = a + b;
+
+//We use printf() function with %d format specifier to display the value of an integer variable.
 printf("value of c : %d \n", c);
+
+printf("%d", c);
 
 f = 70.0/3.0;
 printf("value of f : %f \n", f);
